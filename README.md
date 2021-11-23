7b578a647f719819a18f09fe3e9ff5d2553c482d
Eric Jones <erjones@redhat.com>
2021-11-23 08:54:51 -0500

    feat(config): create template for config file, parse it, and test
    First and most important component to this bot is safely using a config file.

---
+++ README.md
@@ -0,0 +1,5 @@
+# This is a GoLang based Discord bot for my discord server!
+
+This bot is to work on my golang skills as well as update my discord bot!
+
+I will be attempting to use this tool to completely rebuild the existing tool found on Github at [pandabot4Discord](https://github.com/TheDiemer/pandabot4Discord).
---
+++ bot.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Discord struct {
+		Token     string `yaml:"token"`
+		Send_time string `yaml:"send_time"`
+		Owner     string `yaml:"owner"`
+	} `yaml:"discord"`
+	Database struct {
+		IP          string `yaml:"ip"`
+		DB_Username string `yaml:"db_username"`
+		DB_Password string `yaml:"db_password"`
+		DL_Username string `yaml:"dl_username"`
+		DL_Password string `yaml:"dl_password"`
+	}
+}
+
+func main() {
+	conf, err := readFile("conf.yml", "", "yaml")
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+	}
+	fmt.Println(conf)
+}
+
+func readFile(conf_name, conf_path, conf_type string) (config Config, err error) {
+	viper.SetConfigName(conf_name)
+	viper.SetConfigType(conf_type)
+	if conf_path == "" {
+		viper.AddConfigPath(".")
+	}
+	//	viper.AddConfigPath("/home/erjones/gitstuff/GO/discord-go-panda")
+	//	viper.AddConfigPath("/home/jonesin/gitstuff/discord-go-panda")
+	err = viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+	}
+	err = viper.Unmarshal(&config)
+	return
+}
