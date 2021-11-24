0c38a311c96a99b1b8e72b87ef8617e34ff0273d
Eric Jones <erjones@redhat.com>
2021-11-24 10:58:11 -0500

    feat(yaface): start discord bot and start handling messages

--- bot.go
+++ bot.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	//	"encoding/json"
+	//	"flag"
 	"fmt"
+	//	"io/ioutil"
+	//	"net/http"
+	"os"
+	"os/signal"
+	//	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
 	"github.com/spf13/viper"
 )
 
+// Building our config structure so we can use the unmarshalled config values
 type Config struct {
 	Discord struct {
 		Token     string `yaml:"token"`
@@ -17,15 +28,45 @@ type Config struct {
 		DB_Password string `yaml:"db_password"`
 		DL_Username string `yaml:"dl_username"`
 		DL_Password string `yaml:"dl_password"`
-	}
+	} `yaml:"database"`
 }
 
 func main() {
+	// Read and Load up the config file
 	conf, err := readFile("conf.yml", "", "yaml")
+	// Drop out in case it fails to load the config
 	if err != nil {
-		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+		fmt.Println("Fatal error config file: %w \n", err)
+		return
 	}
-	fmt.Println(conf)
+	// fmt.Println(conf.Discord.Token)
+
+	// Create a new Discord session using our bot token
+	dg, err := discordgo.New("Bot " + conf.Discord.Token)
+	// Error checking!
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+
+	dg.AddHandler(messageCreate)
+
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
 }
 
 func readFile(conf_name, conf_path, conf_type string) (config Config, err error) {
@@ -40,4 +81,18 @@ func readFile(conf_name, conf_path, conf_type string) (config Config, err error)
 	}
 	err = viper.Unmarshal(&config)
 	return
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore all messages sent by the bot
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "yaface" {
+		_, err := s.ChannelMessageSend(m.ChannelID, "nahhh, definitely yours :stuck_out_tongue:")
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -2,10 +2,14 @@ module github.com/TheDiemer/discord-go-panda
 
 go 1.17
 
-require github.com/spf13/viper v1.9.0
+require (
+	github.com/bwmarrin/discordgo v0.23.2
+	github.com/spf13/viper v1.9.0
+)
 
 require (
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/gorilla/websocket v1.4.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mitchellh/mapstructure v1.4.2 // indirect
@@ -15,6 +19,7 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
 	golang.org/x/text v0.3.6 // indirect
 	gopkg.in/ini.v1 v1.63.2 // indirect
