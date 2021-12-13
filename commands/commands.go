24615918072beae7a50332a4f6b3b944ad10e401
Eric Jones <erjones@redhat.com>
2021-12-13 14:53:06 -0500

    feat(commands and dnd): expanding command functionality and making rollcall work better
    Roll call now also sends a DM with a request to create a thread and put the attendance chart into
    said thread

--- commands/commands.go
+++ commands/commands.go
@@ -33,15 +33,35 @@ func CommandsHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 				fmt.Println(err)
 			}
 		case "rollcall":
-			var msg strings.Builder
-			if len(args) < 2 {
-				msg = errorMessage("Command missing", "For a list of commands type .help")
-			} else {
-				msg = Attendance(args[1])
-			}
-			_, err := s.ChannelMessageSend(m.ChannelID, msg.String())
-			if err != nil {
-				fmt.Println(err)
+			// Lets make sure this is happening in #dnd or #bots
+			switch m.ChannelID {
+				// #dnd, #bots
+			case "654446991912206346", "655092075389517894":
+				// OKAY
+				var msg strings.Builder
+				var dm strings.Builder
+				if len(args) < 2 {
+					msg = errorMessage("Command missing", "For a list of commands type .help")
+					dm = errorMessage("Command missing", "For a list of commands type .help")
+				} else {
+					msg, dm = Attendance(args[1], m.Author.Mention())
+				}
+				_, err := s.ChannelMessageSend(m.ChannelID, msg.String())
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					user, _ := s.UserChannelCreate(m.Author.ID)
+					_, err := s.ChannelMessageSend(user.ID, dm.String())
+					if err != nil {
+						fmt.Println(err)
+					}
+				}
+			default:
+				msg := errorMessage("Invalid channel", "This command is only available in #dnd (use) or #bots (testing).")
+				_, err := s.ChannelMessageSend(m.ChannelID, msg.String())
+				if err != nil {
+					fmt.Println(err)
+				}
 			}
 
 		default:
