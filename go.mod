50627d3f27d000f7343a43cc5eabd4950516dd20
Eric Jones <TheDiemer@users.noreply.github.com>
2022-03-26 23:03:06 -0400

    Alias (#4)
    * feat(alias slash cmd): alias will be migrated to slash cmd to be better
    
    nest steps are to make the alias backend Actually write to and interact with the db.
    
    * fix(alias upgrade): temp storage on alias upgrade
    
    * fix(aliascall): fixing db writes
    
    Co-authored-by: Eric Jones <erjones@redhat.com>

--- go.mod
+++ go.mod
@@ -8,17 +8,25 @@ require (
 )
 
 require (
+	github.com/go-mysql-org/go-mysql v1.4.0
+	github.com/forPelevin/gomoji v1.1.2 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mitchellh/mapstructure v1.4.2 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/pingcap/errors v0.11.5-0.20201126102027-b0a155152ca3 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/siddontang/go v0.0.0-20180604090527-bdc77568d726 // indirect
+	github.com/siddontang/go-log v0.0.0-20180807004314-8d05993dda07 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
 	golang.org/x/text v0.3.6 // indirect
---
+++ slash/aliasCall.go
@@ -0,0 +1,120 @@
+package slash
+
+import (
+	"fmt"
+	"github.com/TheDiemer/discord-go-panda/config"
+	"github.com/forPelevin/gomoji"
+	"github.com/go-mysql-org/go-mysql/client"
+	"github.com/go-mysql-org/go-mysql/mysql"
+	"strings"
+)
+
+func DeEmoji(sentence string) (replaced strings.Builder) {
+	if gomoji.ContainsEmoji(sentence) {
+		words := strings.Split(sentence, " ")
+		for i, word := range words {
+			info, err := gomoji.GetInfo(word)
+			if err == nil {
+				// This is the condition that means the word was an emoji
+				replaced.WriteString(info.Slug)
+			} else {
+				// And this means it was just a word
+				replaced.WriteString(word)
+			}
+			// Either way we need to reinject spaces
+			if i < len(words)-1 {
+				replaced.WriteString(" ")
+			}
+		}
+	} else {
+		// Since there are no emojis in the sentence, we can return it As is
+		replaced.WriteString(sentence)
+	}
+	return
+}
+
+func Alias(nick string, alias string, conf config.Config) (info strings.Builder, err error) {
+	// the Very First thing that should be done is remove any emoji
+	tmp := DeEmoji(alias)
+	alias = tmp.String()
+	var command strings.Builder
+	// Next, we should check if that alias points to something else
+	command.WriteString("select * from alias where alias = '")
+	command.WriteString(alias)
+	command.WriteString("';")
+	// response, tmpErr := dbGet(conf.Database.IP, conf.Database.DB_Username, conf.Database.DB_Password, "karma", command.String())
+	response, err := dbGet(conf.Database.IP, conf.Database.DB_Username, conf.Database.DB_Password, "karma", command.String())
+	fmt.Println(response.Values)
+	if len(response.Values) > 0 || err != nil {
+		// we got a person back, meaning that alias is in use, or there was an issue with the command itself
+		if len(response.Values) > 0 {
+			tmp, _ := response.GetStringByName(0, "person")
+			err = fmt.Errorf("That alias is already in use!")
+			info.WriteString("\nThat alias is already in use! It currently points to `")
+			info.WriteString(tmp)
+			info.WriteString("`!")
+		} else {
+			info.WriteString("\nAn error occurred while reading the current values:\n`")
+			info.WriteString(err.Error())
+			info.WriteString("`")
+		}
+	} else {
+		fmt.Println("newest here")
+		// either no error AND no people, so lets store this alias
+		var writeCommand strings.Builder
+		writeCommand.WriteString("INSERT INTO alias values('")
+		writeCommand.WriteString(nick)
+		writeCommand.WriteString("', '")
+		writeCommand.WriteString(alias)
+		writeCommand.WriteString("');")
+		err = dbWrite(conf.Database.IP, conf.Database.DB_Username, conf.Database.DB_Password, "karma", writeCommand.String())
+		if err == nil {
+			info.WriteString("\n`")
+			info.WriteString(nick)
+			info.WriteString("` is now known as `")
+			info.WriteString(alias)
+			info.WriteString("`!")
+		} else {
+			info.WriteString("\nSomething happened while saving the new alias:\n`")
+			info.WriteString(err.Error())
+			info.WriteString("`")
+		}
+	}
+	return
+}
+
+// This is to GET data out of the DB, aka read only
+func dbGet(ip string, username string, password string, table string, command string) (response *mysql.Result, err error) {
+	var conn *client.Conn
+	conn, err = client.Connect(ip, username, password, table)
+	if err != nil {
+		return
+	}
+	response, err = conn.Execute(command)
+	fmt.Println("IM HERE1:")
+	fmt.Println(len(response.Values))
+	fmt.Println("still HERE")
+	if err != nil {
+		fmt.Println("IM HERE2:")
+		fmt.Println(err)
+		fmt.Println("IM HERE3:")
+	}
+	return
+}
+
+// This is to PUT data into the DB, aka write
+func dbWrite(ip string, username string, password string, table string, command string) (err error) {
+	// Do things with the command
+	var conn *client.Conn
+	conn, err = client.Connect(ip, username, password, table)
+	if err != nil {
+		return
+	}
+	conn.Ping()
+	r, tmpErr := conn.Execute(command)
+	if tmpErr != nil {
+		err = tmpErr
+	}
+	defer r.Close()
+	return
+}
